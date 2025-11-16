71157d5ad756c485333293b3e5e24cd4e5ba7b31
FranFalcon78 <falcon.fjf@gmail.com>
2025-11-16 01:08:10 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+// Basic cache-first SW (static assets)
+const CACHE = "cursoplayer-web-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate", (e)=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener("fetch", (e)=>{
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+  }
+});
